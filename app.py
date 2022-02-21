ed59e603f1a41bc7adace65d9a88cc508134e790
the-gamers45 <42207280+the-gamers45@users.noreply.github.com>
2022-02-21 16:25:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -73,27 +73,26 @@ def ussd_callback():
                                 "\n  -Amount"
                     ).format(Good_Evening)
 
-    return variables.response
-def balance():
-   
-    mycursor = db.cursor()
-    mycursor.execute('''SELECT primary_phone FROM s_users_primary WHERE primary_phone = (%s)''', (variables.Fetch_Number,))
-    checkEmail = mycursor.fetchall()
-    if variables.text =="balance":
-        if (variables.Fetch_Number,) in checkEmail:
-            mycursor = db.cursor()
-            mycursor.execute('''SELECT first_name FROM s_users_primary WHERE primary_phone = (%s)''', (variables.Fetch_Number,))
-            name = mycursor.fetchone()
-            variables.response =("END Dear {}, your effective balance as at $date is KES $loan_balance."
-
-            ).format(name)
-
-
-        else:
-            variables.response =("END Dear customer, we do not seem to have your details on file. Please visit the office to get registered.")
-    else:
-        variables.response = "END Invalid input. Try again."  
+    def balance():
     
+        mycursor = db.cursor()
+        mycursor.execute('''SELECT primary_phone FROM s_users_primary WHERE primary_phone = (%s)''', (variables.Fetch_Number,))
+        checkEmail = mycursor.fetchall()
+        if variables.text =="balance":
+            if (variables.Fetch_Number,) in checkEmail:
+                mycursor = db.cursor()
+                mycursor.execute('''SELECT first_name FROM s_users_primary WHERE primary_phone = (%s)''', (variables.Fetch_Number,))
+                name = mycursor.fetchone()
+                variables.response =("END Dear {}, your effective balance as at $date is KES $loan_balance."
+
+                ).format(name)
+
+
+            else:
+                variables.response =("END Dear customer, we do not seem to have your details on file. Please visit the office to get registered.")
+        else:
+            variables.response = "END Invalid input. Try again."  
+        return balance
     return variables.response
     
         
