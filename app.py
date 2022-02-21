993f7cfa9b82c9294481a7e638d2851eef720465
the-gamers45 <42207280+the-gamers45@users.noreply.github.com>
2022-02-21 15:46:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import variables
 from maya import MayaInterval
 from dateutil.parser import parse
 from flask import make_response
-import Balance
+import mysql.connector
+
 app = Flask(__name__)
 
 
@@ -16,7 +17,14 @@ api_key = "0f54c06969af94baa76c50efbcc1daaecb9b75f254d3388c85edfd9d21ff7ad0"
 africastalking.initialize(username, api_key)
 
 sms = africastalking.SMS
-
+db = mysql.connector.connect(
+        host = "137.184.54.169",
+        user = "kaguius",
+        passwd = "U6xZfLn9A7Swc%P9",
+        database = "finabora",
+        autocommit = True,
+        port ="3306",
+        )
 
     
 @app.route('/', methods=['POST', 'GET'])
@@ -27,6 +35,8 @@ def ussd_callback():
     session_id = request.values.get("sessionId", None)
     service_code = request.values.get("serviceCode", None)
     phone_number = request.values.get("phoneNumber")
+
+    variables.Fetch_Number = phone_number.split('+')[1]
     text = request.values.get("text", "default")
 
     now = maya.MayaDT.from_datetime(datetime.utcnow())
@@ -60,13 +70,29 @@ def ussd_callback():
                                 "\n  -Amount"
                     ).format(Good_Evening)
 
-    elif variables.text =="Balance":
-        return Balance.balance()
-
-    else:
-        variables.response = "END Invalid input. Try again."
-
     return variables.response
+def balance():
+   
+    mycursor = db.cursor()
+    mycursor.execute('''SELECT primary_phone FROM s_users_primary WHERE primary_phone = (%s)''', (variables.Fetch_Number,))
+    checkEmail = mycursor.fetchall()
+    if variables.text =="Balance":
+        if (variables.Fetch_Number,) in checkEmail:
+            mycursor = db.cursor()
+            mycursor.execute('''SELECT first_name FROM s_users_primary WHERE primary_phone = (%s)''', (variables.Fetch_Number,))
+            name = mycursor.fetchone()
+            variables.response =("END Dear {}, your effective balance as at $date is KES $loan_balance."
+
+            ).format(name)
+
+
+        else:
+            variables.response =("END Dear customer, we do not seem to have your details on file. Please visit the office to get registered.")
+    else:
+        variables.response = "END Invalid input. Try again."  
+    
+    return variables.response
+    
         
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=os.environ.get("PORT"))
